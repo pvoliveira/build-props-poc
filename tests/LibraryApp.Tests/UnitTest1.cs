606e7560ff05b3dbd7536dbbc6e24756a415958c
Paulo Oliveira <paulovt7@gmail.com>
2020-04-08 02:13:44 +0100

    feat: initial solution

---
+++ tests/LibraryApp.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LibraryApp.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
